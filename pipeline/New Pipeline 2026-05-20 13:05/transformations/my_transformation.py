b905ba29c695254d6507e00a91104446d8c21d17
swain.arabind15@gmail.com <swain.arabind15@gmail.com>
2026-05-20 11:19:49 +0000

    comit

---
+++ pipeline/New Pipeline 2026-05-20 13:05/transformations/my_transformation.py
@@ -0,0 +1,8 @@
+from pyspark import pipelines as dp
+
+@dp.table(
+    name = "catalog_dab.schema_dab.dlt_bronze"
+)
+def bronze():
+    df = spark.readStream.table("dlt_catalog.dlt_schema.customer")
+    return df
